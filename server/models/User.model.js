471795c8e4376beaa34848205bb89c175bd5faa3
Diegosch1 <diegoale.rodriguez15@gmail.com>
2024-05-21 17:02:38 -0500

    increase score methos (insanely hard to do)

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,12 +1,16 @@
 import Board from './Board.model.js';
 
 class User {
-    constructor(nickName, type, score, board){
+    constructor(nickName, type, score, board) {
         this.nickName = nickName;
         this.type = type;
-        this.score = score;
+        this.score = 0;
         this.board = board instanceof Board ? board : null;
     }
 }
 
+    function increaseScore() {
+        this.score++;
+    }
+    
 export default User;
